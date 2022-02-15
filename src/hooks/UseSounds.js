4277c8d77113b62f202b36d5a8e916f5f46aeb51
David Gallula <youremail@yourdomain.com>
2022-02-15 18:11:39 +0200

    son imports ok

--- src/hooks/UseSounds.js
+++ src/hooks/UseSounds.js
@@ -1,5 +1,11 @@
 import { useEffect, useRef } from 'react';
-import * as Tone from 'tone'
+import * as Tone from 'tone';
+
+import Loop24 from "Sounds/Loop24.wav";
+import Loop25 from "Sounds/Loop25.wav";
+import Loop26 from "Sounds/Loop26.wav";
+import Loop27 from "Sounds/Loop27.wav";
+
 
 
 export default function UseSounds() {
@@ -9,14 +15,12 @@ export default function UseSounds() {
     //  useEffect with empty dependency array. useEffect(()=>{},[ ])
     useEffect(()=> {
         const sampler = new Tone.Sampler({
-            urls: {
-                "C4": "C4.mp3",
-                "D#4": "Ds4.mp3",
-                "F#4": "Fs4.mp3",
-                "A4": "A4.mp3",
-            },
-            release: 1,
-            baseUrl: "https://tonejs.github.io/audio/salamander/",
+             
+                C4: Loop24,
+                "D#4":Loop25,
+                "F#4":Loop26,
+                A4: Loop27,
+            
         }).toDestination();
 
         Tone.loaded().then(() => {
@@ -29,6 +33,42 @@ export default function UseSounds() {
     function soundPlay(note) {
         mySampler.current.triggerAttackRelease([note],4);
     }
+
+    function handleKeyDown ({key}) {
+        console.log(key);
+
+        switch (key) {
+            case "a":
+            soundPlay("C4")
+            break;   
+            case "b":
+            soundPlay("D#4")
+            break;  
+            case "c":
+            soundPlay("F#4")
+             break; 
+             case "d":
+             soundPlay("A4")
+            break; 
+
+            default:
+                
+        }
+    } 
+
+    function handleKeyDown ({key}) {
+       console.log(key)
+    }
+    
+    useEffect(()=> {
+        window.addEventListener("keydown",handleKeyDown);
+
+        return ()=> {
+            window.removeEventListener("keydown",handleKeyDown);
+        }
+    })
+
+
 
     const buttonsList=
       [
